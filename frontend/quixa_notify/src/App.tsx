870a9943bc91f4d7938dae578e2e50c6f795d6dd
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2023-04-18 12:38:55 -0300

    criando a estrutura para tela de login com rota

--- frontend/quixa_notify/src/App.tsx
+++ frontend/quixa_notify/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import {Links}  from 'react-router-dom'
-import RRoutes from './routes/RRoutes'
+import ViewsRoutes from './routes/ViewsRoutes'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -32,7 +32,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p> */}
       <div>
-        {<RRoutes/>}
+        {<ViewsRoutes/>}
       </div>
     </div>
     
--- frontend/quixa_notify/src/routes/ViewsRoutes.tsx
+++ frontend/quixa_notify/src/routes/ViewsRoutes.tsx
@@ -2,14 +2,16 @@ import {Routes, Route} from "react-router-dom"
 
 import Home from "../views/Home"
 import ReclametionForm from "../components/ReclametionForm"
+import Login from "../views/Login"
 
-const RRoutes = ()=> {
+const ViewsRoutes = ()=> {
     return(
         <Routes>
             <Route path='/' element={<Home></Home>}/> 
             <Route path='/reclameitionform' element={<ReclametionForm></ReclametionForm>} />
+            <Route path='login' element={<Login></Login>} />
         </Routes>
     )
 }
 
-export default RRoutes
+export default ViewsRoutes
