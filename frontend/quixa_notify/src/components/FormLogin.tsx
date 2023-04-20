6076164aa0923a371c319ded837dc231ae952ee9
Edvaldo Oliveira <edvaldooliveira@alu.ufc.br>
2023-04-20 14:47:13 -0300

    algumas correções no FormLogin

--- frontend/quixa_notify/src/components/FormLogin.tsx
+++ frontend/quixa_notify/src/components/FormLogin.tsx
@@ -1,25 +1,52 @@
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 /* eslint-disable prettier/prettier */
 const FormLogin = ()=> {
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const navigate = useNavigate()
+
+    console.log(email, password)
+
+    const handleSubmit =  (event)=> {
+        event.preventDefault()
+        console.log(event)
+    }
+
+
     return  (
         <div className="row  w-100 p-3">
             <div className="col w-100 p-3">
                 <img src="../../public/logo.png" alt="" width={200} height={200}/>
             </div>
-            
-            <div className="col d-flex  flex-column">
+            <form className="col d-flex flex-column " onSubmit={handleSubmit}>
                 <h2>Login</h2>
-                <div className="mb-3">
+                <div className="mb-3 from-group">
                     <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"  
+                            onChange={
+                                (event)=>{
+                                    setEmail(event.target.value)
+                            }
+                        }/>
                 </div>
-                <div className="mb-3">
-                    <label htmlFor="exampleFormControlInput1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
+                <div className="mb-3 form-group">
+                    <label htmlFor="inputPassword5" className="form-label">Password</label>
+                    <input type="password" id="inputPassword5" className="form-control" aria-labelledby="passwordHelpBlock" 
+                            onChange={
+                                (event)=>{
+                                    setPassword(event.target.value)
+                                }
+                            }/>
+                        <div id="passwordHelpBlock" className="form-text">
+                            Your password must be 8-20 characters long
+                        </div>
                 </div>
-                <div className="">
+                <div className="form-group mb-3">
                     <button type="submit" className="btn btn-primary mb-3">Login</button>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
