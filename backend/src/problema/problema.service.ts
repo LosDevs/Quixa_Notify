4947beebd65c026efcd177a7ade2a5763b4832ed
Davi Oliveira <davi.comp.oliveira@gmail.com>
2023-07-04 04:26:49 -0300

    fix: removed unnecessary if condition

--- backend/src/problema/problema.service.ts
+++ backend/src/problema/problema.service.ts
@@ -155,14 +155,12 @@ export class ProblemaService {
 
     async deleteProblema(id, user){
         try {
-        if(id == user.id){
-            const problema = await this.prisma.problema.delete({
-                where :  {
-                    id : Number(`${id}`)
-                },
-            })
-            return { message: "Problema deletado com sucesso", problema }
-        }
+          const problema = await this.prisma.problema.delete({
+              where :  {
+                  id : parseInt(id)
+              },
+          })
+          return { message: "Problema deletado com sucesso", problema }
         } catch (error) {
             return { message: 'Aconteceu algum problema' + error.message }
         }
